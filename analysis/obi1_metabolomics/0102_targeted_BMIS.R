7c47171ceb18eca209b8a3df933e0cc35bad2327
Katherine Heal <katherine.heal@pnnl.gov>
2024-03-12 13:39:14 -0700

    Move functionality for BMIS target to repo

--- analysis/obi1_metabolomics/0102_targeted_BMIS.R
+++ analysis/obi1_metabolomics/0102_targeted_BMIS.R
@@ -1,42 +1,40 @@
-# _____________________________________________________________________________
-#       Copyright 2022, Integral Consulting Inc. All rights reserved.        #
-# _____________________________________________________________________________
-#
-# _____________________________________________________________________________
-#  0102_targeted_BMIS.R
-# _____________________________________________________________________________
-# PURPOSE:
-#  Perform Best Matched Internal Standard Normalization
-#
-#
-# PROJECT INFORMATION:
-#   Name:
-#   Number: C3352
-#
-# HISTORY:
-# 	 Date		        Remarks
-# _____________________________________________________________________________
-# 	 20220420       Using this as a template: https://github.com/kheal/Gradients1_SemiTargeted3/blob/master/SourceCode/BMIS_QEHILIC.R and adapting for these data
-#
+# Purpose: BMIS on all targeted datasets for the Obi1 Metabolomics
+
+# History:
+# Date	     Remarks
+# 20240311   K Heal transferring script from Obi1 specific repo to general repo
+# 20220420       Using this as a template: https://github.com/kheal/Gradients1_SemiTargeted3/blob/master/SourceCode/BMIS_QEHILIC.R and adapting for these data
+
 
 # PACKAGES ----
 library(tidyverse)
 library(here)
-source(here("01_targeted/targeted_dataprocessing_fxns.R"))
+source(here("analysis", "obi1_metabolomics", "functions", "targeted_dataprocessing_fxns.R"))
+
+# SET FILE LOCS ----
+meta_data_loc <- here("data", "raw", "metabolomics", "obi1")
+qc_data_loc <- here("data", "intermediate", "metabolomics", "obi1", "targeted")
+bmis_checks_loc <- here(qc_data_loc, "BMIS_checks")
+
+# MAKE DIR FOR BMIS CHECKS
+dir.create(bmis_checks_loc, recursive = TRUE, showWarnings = FALSE)
 
 # BMIS on HILIC particulate ------
 ## Perform BMIS ----
 BMIS_results <- BMIS(
-  sample_key = "raw_input/sample_key.csv",
-  is_names_file = "raw_input/is_names_particulate.csv",
+  sample_key = here(
+      meta_data_loc,
+      "sample_key.csv"
+  ),
+  is_names_file = here(
+      meta_data_loc,
+      "is_names_particulate.csv"),
   dat_pos_file = here(
-    "01_targeted",
-    "data_intermediate",
+     qc_data_loc,
     "QCd_HILIC_Pos_Particulate.csv"
   ),
   dat_neg_file = here(
-    "01_targeted",
-    "data_intermediate",
+    qc_data_loc,
     "QCd_HILIC_Neg_Particulate.csv"
   ),
   cut_off1 = 0.2,
@@ -52,60 +50,81 @@ BMIS_results <- BMIS(
   is_to_dump = c("AMP, 15N5", "GMP, 15N5", "Succinic acid, 2H4", "Sulfoacetic acid, 13C2")
 )
 ## Save output ----
-### Internal standard reps
-ggsave("01_targeted/data_intermediate/BMIS_checks/HILIC_Particulate_ISreps.pdf",
+### Internal standard reps -----
+ggsave(here(
+    bmis_checks_loc, "HILIC_Particulate_ISreps.pdf") ,
        BMIS_results[[1]],
        width = 15, height = 8, units = "in")
 
-### Quick report
-writeLines(BMIS_results[[2]], "01_targeted/data_intermediate/BMIS_checks/HILIC_Particulate_BMISSummary.txt")
+### Quick report----
+writeLines(BMIS_results[[2]],
+           here(
+               bmis_checks_loc,"HILIC_Particulate_BMISSummary.txt"))
 
-### Internal standard reps
-ggsave("01_targeted/data_intermediate/BMIS_checks/HILIC_Particulate_ISvIScheck.pdf",
+### Internal standard reps----
+ggsave(here(
+    bmis_checks_loc, "HILIC_Particulate_ISvIScheck.pdf"),
        BMIS_results[[3]],
        width = 10, height = 8, units = "in")
 
-### BMIS output
+### BMIS output----
 write_csv(BMIS_results[[4]],
-          "01_targeted/data_intermediate/BMISd_HILIC_Particulate.csv")
-
+          here(qc_data_loc, "BMISd_HILIC_Particulate.csv"))
+print("BMIS complete on HILIC particulate")
 
 
 # BMIS on HILIC dissolved ------
 ## Perform BMIS ----
 BMIS_results <- BMIS(
-  sample_key = "raw_input/sample_key.csv",
-  is_names_file = "raw_input/is_names_dissolved.csv",
-  dat_pos_file = here(
-    "01_targeted",
-    "data_intermediate",
-    "QCd_HILIC_Pos_Dissolved.csv"
-  ),
-  dat_neg_file = here(
-    "01_targeted",
-    "data_intermediate",
-    "QCd_HILIC_Neg_Dissolved.csv"
-  ),
+    sample_key = here(
+        meta_data_loc,
+        "sample_key.csv"
+    ),
+    is_names_file = here(
+        meta_data_loc,
+        "is_names_dissolved.csv"),
+    dat_pos_file = here(
+        qc_data_loc,
+        "QCd_HILIC_Pos_Dissolved.csv"
+    ),
+    dat_neg_file = here(
+        qc_data_loc,
+        "QCd_HILIC_Neg_Dissolved.csv"
+    ),
   cut_off1 = 0.2,
   cut_off2 = 0.1,
   smps_to_dump = c(),
   is_to_dump = c()
 )
 ## Save output ----
-### Internal standard reps
-ggsave("01_targeted/data_intermediate/BMIS_checks/HILIC_Dissolved_ISreps.pdf",
+## Save internal standard reps ----
+ggsave(
+    here(
+        bmis_checks_loc,
+        "HILIC_Dissolved_ISreps.pdf"
+    ),
        BMIS_results[[1]],
        width = 15, height = 8, units = "in")
 
-### Quick report
-writeLines(BMIS_results[[2]], "01_targeted/data_intermediate/BMIS_checks/HILIC_Dissolved_BMISSummary.txt")
+### Quick report-----
+writeLines(BMIS_results[[2]],
+           here(
+               bmis_checks_loc,
+               "HILIC_Dissolved_BMISSummary.txt"
+           ))
 
-### Internal standard reps
-ggsave("01_targeted/data_intermediate/BMIS_checks/HILIC_Dissolved_ISvIScheck.pdf",
+
+### Internal standard reps-----
+ggsave(
+    here(
+        bmis_checks_loc,
+        "HILIC_Dissolved_ISvIScheck.pdf"
+    ),
        BMIS_results[[3]],
        width = 10, height = 8, units = "in")
 
-### BMIS output
+### BMIS output-----
 write_csv(BMIS_results[[4]],
-          "01_targeted/data_intermediate/BMISd_HILIC_Dissolved.csv")
+          here(qc_data_loc, "BMISd_HILIC_Dissolved.csv"))
+print("BMIS complete on HILIC dissolved")
 
--- data/retrieve_metabolomics_data.R
+++ data/retrieve_metabolomics_data.R
@@ -9,10 +9,10 @@ source("R_functions/google_drive_functions.R")
 
 
 # Functions to help get the folder ID needed for the drive_download_daughters_function
-# dir = drive_find(
-#   pattern='OscarSosa_Bacteria',
-#   type='folder',
-#   shared_drive = "Ingalls Lab")
+dir = drive_find(
+  pattern='OscarSosa_Bacteria',
+  type='folder',
+  shared_drive = "Ingalls Lab")
 # query = paste('"', dir$id, '"',  ' in parents', sep='')
 # files = drive_ls(q=query, shared_drive = "Ingalls Lab", recursive = TRUE)
 
@@ -20,31 +20,31 @@ source("R_functions/google_drive_functions.R")
 # Copy OBi1 Metabolomics data from Google Drive -----
 ## mzml data -----
 ### dissolved HILIC Neg mzml-----
-export_folder <- here("data","raw", "metabolomics", "obi1", "dissolved", "mzml", "HILIC_negative_mzml")
+export_folder <- here("data", "raw", "metabolomics", "obi1", "dissolved", "mzml", "HILIC_negative_mzml")
 dir.create(export_folder, recursive = TRUE, showWarnings = FALSE)
 gfolder <- "17c1AaWndGr4TH_aZZH10_N4ytxnBp9A1"
 drive_download_daughters(gfolder, export_folder)
 
 ### dissolved HILIC Pos mzml-----
-export_folder <-  here("data","raw", "metabolomics", "obi1", "dissolved", "mzml", "HILIC_positive_mzml")
+export_folder <- here("data", "raw", "metabolomics", "obi1", "dissolved", "mzml", "HILIC_positive_mzml")
 dir.create(export_folder, recursive = TRUE, showWarnings = FALSE)
 gfolder <- "17btbYSFASS6FuCfTp_3B9c12Jv2SUnka"
 drive_download_daughters(gfolder, export_folder)
 
 ### particulate HILIC Neg mzml-----
-export_folder <-here("data","raw", "metabolomics", "obi1", "particulate",  "mzml", "HILIC_negative_mzml")
+export_folder <- here("data", "raw", "metabolomics", "obi1", "particulate", "mzml", "HILIC_negative_mzml")
 dir.create(export_folder, recursive = TRUE, showWarnings = FALSE)
 gfolder <- "11mQ30CtXReGwuTZdyq0NMEb8kX6471G4"
 drive_download_daughters(gfolder, export_folder)
 
 ### particulate HILIC Pos mzml-----
-export_folder <- here("data","raw", "metabolomics", "obi1", "particulate",  "mzml", "HILIC_positive_mzml")
+export_folder <- here("data", "raw", "metabolomics", "obi1", "particulate", "mzml", "HILIC_positive_mzml")
 dir.create(export_folder, recursive = TRUE, showWarnings = FALSE)
 gfolder <- "11gy9fnvy9l5gQ0RY9JUlKEXyZA-ml3yc"
 drive_download_daughters(gfolder, export_folder)
 
 ### particulate RP mzml-----
-export_folder <- here("data","raw", "metabolomics", "obi1", "particulate",  "mzml", "RP_positive_mzml")
+export_folder <- here("data", "raw", "metabolomics", "obi1", "particulate", "mzml", "RP_positive_mzml")
 dir.create(export_folder, recursive = TRUE, showWarnings = FALSE)
 gfolder <- "1-DUbiFRF7rD7XVO2ST6duSO7NuLMGkdR"
 drive_download_daughters(gfolder, export_folder, refresh_mzml)
@@ -52,43 +52,64 @@ drive_download_daughters(gfolder, export_folder, refresh_mzml)
 
 ## skyline and associated files ------
 ### dissolved skyline and associated  files-----
-export_folder <- here("data","raw", "metabolomics", "obi1",  "dissolved", "skyline")
+export_folder <- here("data", "raw", "metabolomics", "obi1", "dissolved", "skyline")
 dir.create(export_folder, showWarnings = FALSE)
 gfolder <- "12iWeaN5D3m7aUt6Q9sRbBxzTELbSQH_Z"
 drive_download_daughters(gfolder, export_folder)
 
 ### particulate skyline and associated files-----
 # HILIC for all samples and compounds; RP for just internal standards
-export_folder <- here("data","raw", "metabolomics", "obi1",  "particulate", "skyline")
+export_folder <- here("data", "raw", "metabolomics", "obi1", "particulate", "skyline")
 dir.create(export_folder, showWarnings = FALSE)
 gfolder <- "1-EO6dbIP3gb661-HhKfdHxber4IwqwEf"
 drive_download_daughters(gfolder, export_folder)
 
 ## msdial and associated files ------
 ### dissolved HILIC Neg -----
-export_folder <- here("data","raw", "metabolomics", "obi1",  "dissolved", "msdial", "HILIC_negative")
+export_folder <- here("data", "raw", "metabolomics", "obi1", "dissolved", "msdial", "HILIC_negative")
 dir.create(export_folder, showWarnings = FALSE)
 gfolder <- "1ZNoFGayGlipvys00VtF9uE74bXH4SHyJ"
 drive_download_daughters(gfolder, export_folder)
 
 ### dissolved HILIC Pos -----
-export_folder <- here("data","raw", "metabolomics", "obi1",  "dissolved", "msdial", "HILIC_positive")
+export_folder <- here("data", "raw", "metabolomics", "obi1", "dissolved", "msdial", "HILIC_positive")
 dir.create(export_folder, showWarnings = FALSE, recursive = TRUE)
 gfolder <- "1O4VYXDmKtVpy90qeTktv8wV1DO2WeGdG"
 drive_download_daughters(gfolder, export_folder)
 
 ### particulate HILIC Neg -----
-export_folder <- here("data","raw", "metabolomics", "obi1",  "particulate", "msdial", "HILIC_negative")
+export_folder <- here("data", "raw", "metabolomics", "obi1", "particulate", "msdial", "HILIC_negative")
 dir.create(export_folder, showWarnings = FALSE, recursive = TRUE)
 gfolder <- "1rg16L01jW2FHSGzd6_PHNB_kzgpFZ58J"
 drive_download_daughters(gfolder, export_folder)
 ### particulate HILIC Pos -----
-export_folder <- here("data","raw", "metabolomics", "obi1",  "particulate", "msdial", "HILIC_positive")
+export_folder <- here("data", "raw", "metabolomics", "obi1", "particulate", "msdial", "HILIC_positive")
 dir.create(export_folder, showWarnings = FALSE, recursive = TRUE)
 gfolder <- "1rKMrjGk_5KBoip1hDViwcKPJ5vaMbSDt"
 drive_download_daughters(gfolder, export_folder)
 ### particulate RP -----
-export_folder <- here("data","raw", "metabolomics", "obi1",  "particulate", "msdial", "RP_positive")
+export_folder <- here("data", "raw", "metabolomics", "obi1", "particulate", "msdial", "RP_positive")
 dir.create(export_folder, showWarnings = FALSE, recursive = TRUE)
 gfolder <- "1nJzw3bUZ1XNuKvNK89wyKPPn"
 drive_download_daughters(gfolder, export_folder)
+
+# internal standards and sample keys -----
+## clean up the internal standards -----
+# from copied data and clean names for future use
+is_dissolved_dat <- read_csv(here("data", "raw", "metabolomics", "obi1", "dissolved", "skyline", "IS_names_dissolve.csv"),
+  show_col_types = FALSE
+) %>%
+  janitor::clean_names() %>%
+  write_csv(here("data", "raw", "metabolomics", "obi1", "is_names_dissolved.csv"))
+
+is_particulate_dat <- read_csv(here("data", "raw", "metabolomics", "obi1", "particulate", "skyline", "IS_names_particulate.csv"),
+  show_col_types = FALSE
+) %>%
+  janitor::clean_names() %>%
+  write_csv(here("data", "raw", "metabolomics", "obi1", "is_names_particulate.csv"))
+
+## grab sample key -----
+gfile <- drive_get(as_id("1KL7-kJOZrlDtV5wToq_ejkLNtBiGNC5Y"), shared_drive = "Ingalls Lab")
+drive_download(gfile,
+               path = here("data", "raw", "metabolomics", "obi1", "sample_key.csv"))
+
