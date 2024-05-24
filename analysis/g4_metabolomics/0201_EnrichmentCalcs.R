4cb70e11f22caa087a3a7da2ff0ad2ab41930932
Katherine Heal <katherine.heal@pnnl.gov>
2024-05-24 16:34:04 -0700

    Add G4 metabolomics data through enrichment calculations

--- analysis/g4_metabolomics/0201_EnrichmentCalcs.R
+++ analysis/g4_metabolomics/0201_EnrichmentCalcs.R
@@ -1,5 +1,4 @@
-# PURPOSE: calculate enrichment factors for the Obi1 samples
-# 20240313  KRH moving from Obi1-specific repo
+# PURPOSE: calculate enrichment factors for the G4 samples
 
 # PACKAGES & SPECIAL FUNCTIONS ----
 library(tidyverse)
@@ -11,7 +10,7 @@ library(here)
 
 # SET FILE LOCATIONS  -----
 meta_data_dir <- here("data", "intermediate", "metabolomics")
-output_loc <- here("data", "intermediate", "metabolomics", "rpom")
+output_loc <- here("data", "intermediate", "metabolomics", "g4")
 
 # Read in files  -----
 dat_long_filename <- here(
@@ -31,7 +30,11 @@ dat_cmb <- dat_cmb %>%
     filter(!all(is.na(adjusted_area))) %>%
     mutate(adjusted_area = ifelse(is.na(adjusted_area), 0.02*min(adjusted_area, na.rm = TRUE), adjusted_area)) %>%
     ungroup() %>%
-    mutate(replicate_name_short = str_remove(replicate_name, "^\\d\\d\\d\\d\\d\\d_"))
+    mutate(replicate_name_short = str_remove(replicate_name, "^\\d\\d\\d\\d\\d\\d_")) %>%
+    # drop core_metab of Glutathione disulfide
+    filter(mass_feature != "Glutathione disulfide") %>%
+    # drop all _D2 mass features
+    filter(!grepl("_D2", mass_feature))
 
 
 ## Pull out core metab ------
@@ -66,33 +69,24 @@ dat_enrich <- bind_rows(dat_enrich_list) %>%
 
 ## Calculate per-sample ttest and mann-whitney test for each compound x core_metab combo ------
 dat_enrich_h_tests <- dat_enrich %>%
-    select(mass_feature, core_metab, scaled_area, treatment, sample_fraction) %>%
-    filter(treatment %in% c("Glucose", "Homarine")) %>%
-    group_by(mass_feature, core_metab, sample_fraction) %>%
+    filter(timepoint > 0 ) %>%
+    select(mass_feature, core_metab, scaled_area, treatment, experiment_id, timepoint, sample_fraction) %>%
+    filter(treatment %in% c("Control", "Homarine")) %>%
+    group_by(mass_feature, core_metab, sample_fraction, experiment_id, timepoint) %>%
     summarise(ttest_p_h = t.test(scaled_area ~ treatment, paired = FALSE)$p.value,
               p_value_mannwhitney_h = wilcox.test(scaled_area ~ treatment)$p.value)
 
-dat_enrich_hNH4_tests <- dat_enrich %>%
-    select(mass_feature, core_metab, scaled_area, treatment, sample_fraction) %>%
-    filter(treatment %in% c("Glucose and homarine", "Glucose")) %>%
-    group_by(mass_feature, core_metab, sample_fraction) %>%
-    summarise(ttest_p_hNH4 = t.test(scaled_area ~ treatment, paired = FALSE)$p.value,
-              p_value_mannwhitney_hNH4 = wilcox.test(scaled_area ~ treatment)$p.value)
-
-dat_enrich_tests <- left_join(dat_enrich_h_tests, dat_enrich_hNH4_tests, by = c("mass_feature", "core_metab", "sample_fraction"))
-
-
-## Calculate fold changes for each compound x core_metab combo  ------
+## Calculate fold changes for each compound x core_metab x timepoint x experiment combo  ------
 dat_enrich3 <- dat_enrich %>%
-    group_by(mass_feature, treatment, sample_fraction, core_metab) %>%
+    filter(!is.na(treatment)) %>%
+    group_by(mass_feature, treatment, experiment_id, timepoint, sample_fraction, core_metab) %>%
     summarise(mean_scaled_area = mean(scaled_area, rm.na = T),
               mean_adjusted_area = mean(adjusted_area, rm.na = T)) %>%
     ungroup() %>%
-    group_by(mass_feature, core_metab, sample_fraction) %>%
-    summarise(enrich_hNH4_fc = mean_scaled_area[treatment == "Glucose and homarine"] /  mean_scaled_area[treatment == "Glucose"],
-              enrich_h_fc = mean_scaled_area[treatment == "Homarine"] / mean_scaled_area[treatment == "Glucose"])%>%
-    ungroup() %>%
-    left_join(dat_enrich_tests, by = c("mass_feature", "core_metab", "sample_fraction"))
+    group_by(mass_feature, core_metab, sample_fraction, experiment_id, timepoint) %>%
+    reframe(enrich_h_fc = mean_scaled_area[treatment == "Homarine"] / mean_scaled_area[treatment == "Control"])%>%
+   # ungroup() %>%
+    left_join(dat_enrich_h_tests, by = c("mass_feature", "core_metab", "sample_fraction", "experiment_id", "timepoint"))
 
 ## Test for outliers among core metabolites ------
 core_outlier_tests <- list()
@@ -100,12 +94,9 @@ for (i in 1:length(unique(dat_enrich3$mass_feature))){
     # test if any of the core_metab values are outliers for the fold change
     core_outlier_tests[[i]] <- dat_enrich3 %>%
         filter(mass_feature == unique(dat_enrich3$mass_feature)[i]) %>%
+        group_by(experiment_id, timepoint) %>%
         # Calculate the grubb's test for outliers
-        mutate(outlier_hNH4 =
-                   ifelse(
-                       abs(enrich_hNH4_fc - mean(enrich_hNH4_fc)) > 2*sd(enrich_hNH4_fc),
-                       TRUE, FALSE),
-               outlier_h =
+        mutate(outlier_h =
                    ifelse(
                        abs(enrich_h_fc - mean(enrich_h_fc)) > 2*sd(enrich_h_fc),
                        TRUE, FALSE)
@@ -114,14 +105,15 @@ for (i in 1:length(unique(dat_enrich3$mass_feature))){
 }
 core_outlier_tests <- bind_rows(core_outlier_tests)
 core_outliers <- core_outlier_tests %>%
+    group_by(core_metab, experiment_id, timepoint) %>%
+    summarise(outlier_f_h = sum(outlier_h,  na.rm = T)/n()) %>%
+    ungroup() %>%
     group_by(core_metab) %>%
-    summarise(outlier_f_hNH4 = sum(outlier_hNH4, na.rm = T)/n(),
-              outlier_f_h = sum(outlier_h,  na.rm = T)/n()) %>%
-    ungroup()
+    summarise(outlier_f_h = mean(outlier_f_h, na.rm = T))
 
 # pull core metabs in which less than 10% of the values are outliers
 core_metabs_quality <- core_outliers %>%
-    filter(outlier_f_hNH4 < 0.1 & outlier_f_h < 0.1) %>%
+    filter( outlier_f_h < 0.1) %>%
     filter(!is.na(core_metab))
 write_csv(core_metabs_quality, here(output_loc, "core_metabs_quality.csv"))
 
@@ -132,19 +124,14 @@ write_csv(dat_enrich4, here(output_loc, "enrichment_results.csv"))
 
 ## Summarize enrichment results ------
 dat_enrich5 <- dat_enrich4 %>%
-    group_by(mass_feature, sample_fraction) %>%
+    group_by(mass_feature, sample_fraction, experiment_id, timepoint) %>%
     summarise(
         med_pvalue_h = median(ttest_p_h),
-        med_pvalue_hNH4 = median(ttest_p_hNH4),
         med_fc_h = median(enrich_h_fc),
-        med_fc_hNH4 = median(enrich_hNH4_fc),
-        med_pvalue_cmb = med_pvalue_h*med_pvalue_hNH4
     ) %>%
     ungroup() %>%
     mutate(
         med_qvalue_h = p.adjust(med_pvalue_h, method = "fdr"),
-        med_qvalue_hNH4 = p.adjust(med_pvalue_hNH4, method = "fdr"),
-        med_qvalue_cmb = p.adjust(med_pvalue_cmb, method = "fdr")
     ) %>%
     mutate(core_metabolite = ifelse(mass_feature %in% core_metabs_quality$core_metab, "core", "non-core"))
 write_csv(dat_enrich5, here(output_loc, "enrichment_summary.csv"))
