9204d83833f68aeb82390335856eff0a0172c09a
Katherine Heal <katherine.heal@pnnl.gov>
2024-08-23 12:40:45 -0700

    Update renv and style table script

--- analysis/obi1_metabolomics/0303_SummaryTable.R
+++ analysis/obi1_metabolomics/0303_SummaryTable.R
@@ -5,7 +5,7 @@
 library(tidyverse)
 library(janitor)
 library(here)
-library(flextable)
+library(openxlsx2)
 
 
 # SET FILE LOCATIONS  -----
@@ -25,59 +25,59 @@ mf_info <- here(output_loc, "combined_MF_info.csv") %>%
 sample_key <- read_csv(sample_key, show_col_types = FALSE)
 
 
-for (sample_fraction_oi in c("Particulate", "Dissolved")){
-
-# SUMMARIZE DATA ----
-# Prepare raw data (pivot to wide)
-supp_t <- raw_data %>%
+for (sample_fraction_oi in c("Particulate", "Dissolved")) {
+  # SUMMARIZE DATA ----
+  # Prepare raw data (pivot to wide)
+  supp_t <- raw_data %>%
     filter(sample_set == "OBi1_set2") %>%
     filter(sample_fraction == sample_fraction_oi) %>%
     select(mass_feature, replicate_name, adjusted_area) %>%
-  pivot_wider(names_from = replicate_name, values_from = adjusted_area) %>%
+    pivot_wider(names_from = replicate_name, values_from = adjusted_area) %>%
     drop_na()
 
-# Add in mass feature info
-supp_t2 <- supp_t %>%
-  left_join(mf_info %>%
-                filter(sample_fraction == sample_fraction_oi), by = "mass_feature") %>%
+  # Add in mass feature info
+  supp_t2 <- supp_t %>%
+    left_join(mf_info %>%
+      filter(sample_fraction == sample_fraction_oi), by = "mass_feature") %>%
     select(sample_fraction, mass_feature, z, RT, mz, MS2, add_annotation, starts_with("2"))
 
-# Add in enrichment data
-supp_t3 <- supp_t2 %>%
-  left_join(enrichment_data, by = join_by(sample_fraction, mass_feature)) %>%
+  # Add in enrichment data
+  supp_t3 <- supp_t2 %>%
+    left_join(enrichment_data, by = join_by(sample_fraction, mass_feature)) %>%
     relocate(core_metabolite, .after = mass_feature) %>%
     select(-med_pvalue_cmb, -med_qvalue_h, -med_qvalue_hNH4, -med_qvalue_cmb) %>%
     select(sample_fraction:add_annotation, starts_with("med"), starts_with("2")) %>%
     select(-sample_fraction) %>%
     arrange(z, mz)
 
-# Prepare the table for saving as an excel file
-supp_t4 <- supp_t3 %>%
-    rename(`Mass Feature` = mass_feature,
-           `Charge` = z,
-           `Retention Time (min)` = RT,
-           `m/z` = mz,
-           `MS2` = MS2,
-           `Adduct Annotation` = add_annotation,
-           `Core Metabolite` = core_metabolite,
-           `p (Glucose + NH4 + Homarine)` = med_pvalue_hNH4,
-           `p (Homarine)` = med_pvalue_h,
-           `fold change (Glucose + NH4 + Homarine)` = med_fc_hNH4,
-           `fold change (Homarine)` = med_fc_h)
+  # Prepare the table for saving as an excel file
+  supp_t4 <- supp_t3 %>%
+    rename(
+      `Mass Feature` = mass_feature,
+      `Charge` = z,
+      `Retention Time (min)` = RT,
+      `m/z` = mz,
+      `MS2` = MS2,
+      `Adduct Annotation` = add_annotation,
+      `Core Metabolite` = core_metabolite,
+      `p (Glucose + NH4 + Homarine)` = med_pvalue_hNH4,
+      `p (Homarine)` = med_pvalue_h,
+      `fold change (Glucose + NH4 + Homarine)` = med_fc_hNH4,
+      `fold change (Homarine)` = med_fc_h
+    )
 
 
-# Prep sample_key
-sample_key <- sample_key %>%
-  filter(replicate_name %in% colnames(supp_t)) %>%
+  # Prep sample_key
+  sample_key <- sample_key %>%
+    filter(replicate_name %in% colnames(supp_t)) %>%
     select(replicate_name, treatment)
 
 
 
-# Write it to an excel workbook
-library(openxlsx2)
-sheet_name <- paste0("Obi1 ", sample_fraction_oi, " Metab Results")
-row_end <- nrow(supp_t4) + 3
-wb <- openxlsx2::wb_workbook() %>%
+  # Write it to an excel workbook
+  sheet_name <- paste0("Obi1 ", sample_fraction_oi, " Metab Results")
+  row_end <- nrow(supp_t4) + 3
+  wb <- openxlsx2::wb_workbook() %>%
     wb_add_worksheet(sheet_name) %>%
     # Add the table
     wb_add_data(supp_t4, start_row = 3, sheet = sheet_name) %>%
@@ -91,7 +91,6 @@ wb <- openxlsx2::wb_workbook() %>%
     wb_add_data("Adjusted areas", start_row = 1, start_col = 12, sheet = sheet_name) %>%
     wb_merge_cells(dims = wb_dims(rows = 1:1, cols = 12:20), sheet = sheet_name) %>%
     wb_add_border(dims = wb_dims(rows = 2:row_end, cols = 12:20), sheet = sheet_name) %>%
-
     # Add treatments for the enrichment data
     wb_add_data("Glucose + NH4", start_row = 2, start_col = 12, sheet = sheet_name) %>%
     wb_merge_cells(dims = wb_dims(rows = 2:2, cols = 12:14), sheet = sheet_name) %>%
@@ -102,30 +101,23 @@ wb <- openxlsx2::wb_workbook() %>%
     wb_add_data("Glucose + NH4 + Homarine", start_row = 2, start_col = 18, sheet = sheet_name) %>%
     wb_merge_cells(dims = wb_dims(rows = 2:2, cols = 18:20), sheet = sheet_name) %>%
     wb_add_border(dims = wb_dims(rows = 2:row_end, cols = 18:20), sheet = sheet_name) %>%
-
     wb_add_border(dims = wb_dims(rows = 1:2, cols = 1:20), sheet = sheet_name) %>%
     wb_add_border(dims = wb_dims(rows = 2:2, cols = 12:20), sheet = sheet_name) %>%
-
     # Set the column widths
     wb_set_col_widths(cols = c(1:5, 7:20), widths = "auto", sheet = sheet_name) %>%
     # Make bold and center top 2 rows and first column
-    wb_add_font(dims = wb_dims(rows = 1:3, cols = 1:20), bold= "double", sheet = sheet_name) %>%
-    wb_add_font(dims = wb_dims(rows = 1:row_end, cols = 1), bold= "single", sheet = sheet_name) %>%
+    wb_add_font(dims = wb_dims(rows = 1:3, cols = 1:20), bold = "double", sheet = sheet_name) %>%
+    wb_add_font(dims = wb_dims(rows = 1:row_end, cols = 1), bold = "single", sheet = sheet_name) %>%
     # Center align the top 2 rows
     wb_add_cell_style(
-        wb_dims(rows = 1:2, cols = 1:20),
-        horizontal = "center",
-        sheet = sheet_name
+      wb_dims(rows = 1:2, cols = 1:20),
+      horizontal = "center",
+      sheet = sheet_name
     )
-    # Add a border
+  # Add a border
 
 
-# Save the workbook
-save_path <- here(save_loc, paste0("Obi1_", sample_fraction_oi, "_metab_results.xlsx"))
-wb$save(save_path)
-
+  # Save the workbook
+  save_path <- here(save_loc, paste0("Obi1_", sample_fraction_oi, "_metab_results.xlsx"))
+  wb$save(save_path)
 }
-
-
-
-
