806858f2bceab24e37371223b6afa66721ca6615
Katherine Heal <katherine.heal@pnnl.gov>
2025-01-06 10:38:31 -0800

    WIP add rc104 QC and set up for analysis steps

---
+++ data/retrieve_metabolomics_data_PS.R
@@ -0,0 +1,38 @@
+# Purpose: Script for populating data folders after project initialization
+
+# History:
+# Date	        Remarks
+# 20250106      K Heal modifying to pull RC104 Homarine fate experiment metabolomics data
+
+library(tidyverse)
+source("R_functions/google_drive_functions.R")
+
+
+# Functions to help get the folder ID needed for the drive_download_daughters_function
+#dir <- drive_find(
+#    pattern = "RC104_HomarineFate",
+#    type = "folder",
+#    shared_drive = "Ingalls Lab"
+#)
+# query = paste('"', dir$id, '"',  ' in parents', sep='')
+# query = "\"1-xt3TXKEtClt5WUEIHc5nMgfhNoompuo\" in parents"
+#files = drive_ls(q=query, shared_drive = "Ingalls Lab", recursive = TRUE)
+
+# Copy RPom Metabolomics data from Google Drive -----
+## mzml data -----
+### dissolved HILIC Neg mzml-----
+export_folder <- here("data", "raw", "metabolomics", "RC104", "particulate","mzml", "HILIC_negative")
+dir.create(export_folder, recursive = TRUE, showWarnings = FALSE)
+gfolder <- "1JDoNK2W2ps3twLP0VuzaCev_wtlP2cjt"
+drive_download_daughters(gfolder, export_folder)
+
+### dissolved HILIC Pos mzml-----
+export_folder <- here("data", "raw", "metabolomics", "RC104", "particulate","mzml", "HILIC_positive")
+dir.create(export_folder, recursive = TRUE, showWarnings = FALSE)
+gfolder <- "16oCaT5FE4dnn6FY9qqbJhBQ6TF5hollC"
+drive_download_daughters(gfolder, export_folder)
+
+## grab sample key -----
+#gfile <- drive_get(as_id("15AMo5diyaLdvJIeagqATJQOYt_6WVyY3"), shared_drive = "Ingalls Lab")
+#drive_download(gfile,
+#               path = here("data", "raw", "metabolomics", "RC104", "sample_key.csv"))
