e4fb4343fc00785e34e14bf76e97195468def7e3
Katherine Heal <katherine.heal@pnnl.gov>
2025-01-29 10:31:17 -0800

    Clean up Figure SX1

--- analysis/obi1_metabolomics/0401_Plot_FC_particulate.R
+++ analysis/obi1_metabolomics/0401_Plot_FC_particulate.R
@@ -9,10 +9,10 @@ library(ggrepel)
 
 # Set file locs ------
 data_dir <- here("data", "intermediate", "metabolomics", "obi1")
-figure_dir <- here("figures", "exploratory", "metabolomics", "obi1")
+figure_dir <- here("figures", "metabolomics")
 
 # Read in files  -----
-dat_MF_filename <- here(data_dir, "combined_MF_info.csv") #PARTICULATE ONLY
+dat_MF_filename <- here(data_dir, "combined_MF_info.csv") # PARTICULATE ONLY
 dat_MF_enrichment_filename <- here(data_dir, "enrichment_results.csv")
 
 dat_MF <-
@@ -28,61 +28,73 @@ pseudos <- dat_MF %>%
 
 # SUMMARY TABLES------
 dat_enrich_summary <- dat_enrichment_results %>%
-    inner_join(pseudos, by = c("mass_feature", "sample_fraction")) %>%
+  inner_join(pseudos, by = c("mass_feature", "sample_fraction")) %>%
   group_by(mass_feature, sample_fraction) %>%
-    summarise(
-      med_pvalue_h = median(ttest_p_h),
-      med_pvalue_hNH4 = median(ttest_p_hNH4),
-      med_fc_h = median(enrich_h_fc),
-      med_fc_hNH4 = median(enrich_hNH4_fc),
-      med_pvalue_cmb = med_pvalue_h*med_pvalue_hNH4
-    ) %>%
-    ungroup() %>%
-    mutate(
-        med_qvalue_h = p.adjust(med_pvalue_h, method = "fdr"),
-        med_qvalue_hNH4 = p.adjust(med_pvalue_hNH4, method = "fdr"),
-        med_qvalue_cmb = p.adjust(med_pvalue_cmb, method = "fdr")
-    ) %>%
-    left_join(dat_MF, by = c("mass_feature", "sample_fraction")) %>%
-    mutate(plot_label =
-               case_when(
-                   !str_detect(mass_feature, "HILIC") ~ mass_feature,
-                   str_detect(mass_feature, "HILICPos") ~ paste0("\nRT = ", round(RT, digits = 1), "\n mz = ", round(mz, digits = 3), "+"),
-                   str_detect(mass_feature, "HILICNeg") ~ paste0("\nRT = ", round(RT, digits = 1), "\n mz = ", round(mz, digits = 3), "-")
-               ))
+  summarise(
+    med_pvalue_h = median(ttest_p_h),
+    med_pvalue_hNH4 = median(ttest_p_hNH4),
+    med_fc_h = median(enrich_h_fc),
+    med_fc_hNH4 = median(enrich_hNH4_fc),
+    med_pvalue_cmb = med_pvalue_h * med_pvalue_hNH4
+  ) %>%
+  ungroup() %>%
+  mutate(
+    med_qvalue_h = p.adjust(med_pvalue_h, method = "fdr"),
+    med_qvalue_hNH4 = p.adjust(med_pvalue_hNH4, method = "fdr"),
+    med_qvalue_cmb = p.adjust(med_pvalue_cmb, method = "fdr")
+  ) %>%
+  left_join(dat_MF, by = c("mass_feature", "sample_fraction")) %>%
+  mutate(
+    plot_label =
+      case_when(
+        str_detect(mass_feature, "HILICPos") ~ paste0("\nRT = ", round(RT, digits = 1), "\n mz = ", round(mz, digits = 3), "+"),
+        str_detect(mass_feature, "HILICNeg") ~ paste0("\nRT = ", round(RT, digits = 1), "\n mz = ", round(mz, digits = 3), "-"),
+        str_detect(mass_feature, "^unknown") &
+            str_detect(mass_feature, "_pos$") ~ paste0("\nRT = ", round(RT, digits = 1), "\n mz = ", round(mz, digits = 3), "-"),
+        str_detect(mass_feature, "^unknown") &
+            str_detect(mass_feature, "_neg$") ~ paste0("\nRT = ", round(RT, digits = 1), "\n mz = ", round(mz, digits = 3), "-"),
+        TRUE ~ mass_feature
+      )
+  ) %>%
+    # drop infinite values
+    filter(med_fc_h != Inf & med_fc_hNH4 != Inf)
 
 
 set.seed(123)
 g4 <- ggplot(
-    data = dat_enrich_summary,
-    aes(
-        x = log2(med_fc_h),
-        y = log2(med_fc_hNH4),
-        color = sample_fraction,
-        shape = sample_fraction
-    )
+  data = dat_enrich_summary,
+  aes(
+    x = log2(med_fc_h),
+    y = log2(med_fc_hNH4),
+    color = sample_fraction,
+    shape = sample_fraction
+  )
 ) +
-    geom_point() +
-    gghighlight(med_pvalue_h < 0.05 & log2(med_fc_h) > 0,
-                use_direct_label = FALSE) +
-    geom_text_repel(
-        aes(label = plot_label),
-        colour = "black",
-        size = 2,
-        )+
-    theme_bw() +
-    labs(
-        x = "Median enrichment factor in homarine treatment (log2)",
-        y = "Median enrichment factor in homarine + NH4 treatment (log2)",
-    ) +
-    scale_color_manual(values = c("aquamarine3", "darkorchid3", "black")) +
-    scale_shape_manual(values = c(17, 16)) +
-    theme(legend.position = c(0.1, 0.8),
-          legend.title = element_blank(),
-          legend.background = element_rect(fill = "white", colour = "black"),
-          text = element_text(size = 7))
+  geom_point() +
+  gghighlight(med_pvalue_h < 0.05 & log2(med_fc_h) > 0,
+    use_direct_label = FALSE
+  ) +
+  geom_text_repel(
+    aes(label = plot_label),
+    colour = "black",
+    size = 2,
+    min.segment.length = unit(0, 'lines')
+  ) +
+  theme_bw() +
+  labs(
+    x = "Median enrichment factor in homarine vs glucose (log2)",
+    y = "Median enrichment factor in homarine+glucose vs glucose (log2)",
+  ) +
+  scale_color_manual(values = c("aquamarine3", "darkorchid3", "black")) +
+  scale_shape_manual(values = c(17, 16)) +
+  theme(
+    legend.position = c(0.1, 0.8),
+    legend.title = element_blank(),
+    legend.background = element_rect(fill = "white", colour = "black"),
+    text = element_text(size = 7)
+  )
 
 plot(g4)
 
 # SAVE OUTPUTS -----
-ggsave(here(figure_dir, "EnrichmentFactors_scatter_psuedos.pdf"), plot = g4, height = 5, width = 6, units = "in")
+ggsave(here(figure_dir, "Figure_SX1_Obi1_EnrichmentFactors_scatter.pdf"), plot = g4, height = 6, width = 8, units = "in")
