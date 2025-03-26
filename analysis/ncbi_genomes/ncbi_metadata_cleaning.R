f305eed5e3f63d0aaf734fd92a891334f997e953
Katherine Heal <katherine.heal@pnnl.gov>
2025-03-26 16:19:32 -0700

    Expand lat/long lookup for ncbi genomes

--- analysis/ncbi_genomes/ncbi_metadata_cleaning.R
+++ analysis/ncbi_genomes/ncbi_metadata_cleaning.R
@@ -5,6 +5,8 @@
 # Libraries -----
 library(tidyverse)
 library(here)
+library(httr)
+library(jsonlite)
 
 # Special Functions-----
 ## function to parse lat_lon into lat and lon for plotting
@@ -26,11 +28,57 @@ parse_lat_lon <- function(lat_lon) {
   return(c(lat, lon))
 }
 
+## function to convert lat or lon from DMS to decimal
+convert_lat_lon <- function(lat) {
+  # split lat_lon into lat, dir, lon, dir2
+  lat_split <- str_split(lat, " ")
+  dir <- lat_split[[1]][1]
+  lat_deg <- parse_number(lat_split[[1]][2])
+  lat_min <- parse_number(lat_split[[1]][3])
+  lat_sec <- parse_number(lat_split[[1]][4])
+  lat_dec <- lat_deg + lat_min / 60 + lat_sec / 3600
+  if (dir == "S") {
+    lat_dec <- -lat_dec
+  }
+  if (dir == "W") {
+    lat_dec <- -lat_dec
+  }
+  return(lat_dec)
+}
+ 
+# Read in and parse lookup -------
+geo_loc_lookup <- read_csv(here("data", "intermediate", "ncbi_metadata_geo_loc_name_lookup.csv"),
+  show_col_types = FALSE
+) %>%
+    filter(!is.na(lat) & !is.na(lon)) %>%
+    select(geo_loc_name, lat, lon) %>%
+    mutate(lat_new = NA_real_, lon_new = NA_real_)
+
+# parse lat_lon in lookup using parse_lat_lon2
+for (i in 1:nrow(geo_loc_lookup)) {
+  lat_new <- convert_lat_lon(geo_loc_lookup$lat[i])
+  lon_new <- convert_lat_lon(geo_loc_lookup$lon[i])
+  geo_loc_lookup$lat_new[i] <- lat_new
+  geo_loc_lookup$lon_new[i] <- lon_new
+}
+geo_loc_lookup <- geo_loc_lookup %>%
+  select(-lat, -lon) %>%
+  rename(lat = lat_new, lon = lon_new) %>%
+    mutate(location_qual = "derived from geo_loc_name")
+
 # Get Data -----
 dat <- read_csv(here("data", "intermediate", "ncbi_metadata_biosample_info.csv"),
   show_col_types = FALSE
 ) %>%
     mutate(isolation_source = tolower(isolation_source))
+
+genomes_oi <- read_delim(here("data", "intermediate", "ncbi_genome_counts", "ncbi_complete_homarine_operons_loci_with_taxonomy.txt"), delim = "\t",
+  show_col_types = FALSE
+)
+
+## filter data to only those in genomes_oi
+dat <- dat %>%
+  filter(assembly_accession %in% genomes_oi$Assembly)
 
 # get unique isolation_source with counts
 unique_isolation_source <- dat %>%
@@ -115,8 +163,8 @@ iso_source_mapped <- unique_isolation_source %>%
   mutate(isolation_source = tolower(isolation_source)) %>%
   mutate(isolation_source_mapped = case_when(
     is.na(isolation_source) ~ "unknown",
-    str_detect(isolation_source, paste(non_environment_isolation_source, collapse = "|")) ~ "non-environmental",
     str_detect(isolation_source, paste(environment_isolation_source, collapse = "|")) ~ "environmental",
+    str_detect(isolation_source, paste(non_environment_isolation_source, collapse = "|")) ~ "non-environmental",
     str_detect(isolation_source, paste(unknown_isolation_source, collapse = "|")) ~ "unknown",
     TRUE ~ "unknown"
   )) %>%
@@ -137,6 +185,18 @@ dat2 <- dat %>%
         by = "isolation_source"
     )
 
+# Map bioproject to categories ----
+## Read in bioproject lookup table -----
+bioproject_lookup <- read_csv(here("data", 
+                                   "intermediate",
+                                   "ncbi_genome_counts",
+                                   "ncbi_bioproject_lookup.csv"),
+                              show_col_types = FALSE)
+
+# update records with bioproject-level annotations of interest
+dat2 <- dat2 %>%
+    rows_update(bioproject_lookup, by = "bioproject_accession")
+  
 # Add lat_lon for environmental samples -----
 ## Generate lat lon lookup table -----
 lat_lon_lookup <- dat2 %>%
@@ -155,11 +215,83 @@ for (i in 1:nrow(lat_lon_lookup)) {
 
 ## Apply lat lon lookup  -----
 dat3 <- dat2 %>%
-  left_join(lat_lon_lookup, by = "lat_lon") %>%
-    select(
-        taxon_id, assembly_accession, isolation_source, lat_lon, 
-        isolation_source_mapped, environmental_source_mapped, lat, lon
-        )
+  left_join(lat_lon_lookup, by = "lat_lon")
+
+## Use and apply `geographic location (latitude)`, `geographic location (longitude)`
+# if lat_lon is missing
+# Go from ~30% coverage to 37% coverage
+dat3 <- dat3 %>%
+  mutate(lat = case_when(
+    !is.na(lat) ~ lat,
+    !is.na(`geographic location (latitude)`) ~ as.numeric(`geographic location (latitude)`),
+    TRUE ~ NA_real_
+  )) %>%
+  mutate(lon = case_when(
+    !is.na(lon) ~ lon,
+    !is.na(`geographic location (longitude)`) ~ as.numeric(`geographic location (longitude)`),
+    TRUE ~ NA_real_
+  )) %>%
+    # if lat is out of range, replace with NA
+    mutate(lat = case_when(
+      lat < -90 | lat > 90 ~ NA_real_,
+      lon < -180 | lon > 180 ~ NA_real_,
+      TRUE ~ lat
+    )) %>%
+    # if lon is out of range, replace with NA
+    mutate(lon = case_when(
+      lon < -180 | lon > 180 ~ NA_real_,
+      lat < -90 | lat > 90 ~ NA_real_,
+      TRUE ~ lon
+    ))
+
+## Apply lat lon lookup from geo_loc_name -----
+### Pull out records with missing lat_lon and attempt to fill in with geo_loc_name lookup
+dat3_no_lat_lon <- dat3 %>%
+  filter(is.na(lat) | is.na(lon)) %>%
+    select(-lat, -lon) %>%
+    left_join(geo_loc_lookup, by = "geo_loc_name")
+
+# nearly 80% coverage for lat lon!
+dat4 <- dat3 %>%
+    filter(!is.na(lat) | !is.na(lon)) %>%
+    bind_rows(dat3_no_lat_lon)
+
+## Summarize data -----
+dat_summary <- dat4 %>%
+  group_by(isolation_source_mapped, environmental_source_mapped) %>%
+  summarize(n = n(),
+            n_with_lat_lon = sum(!is.na(lat) & !is.na(lon)),
+            n_with_lat_lon_pct = n_with_lat_lon / n(),
+            ) %>%
+  arrange(desc(n))
+
+# Get a df of just the environmental samples -----
+#only about 30% of these have lat_lon data :(
+dat5 <- dat4 %>%
+    filter(isolation_source_mapped == "environmental")
+
+# Make a plot of dat4, colored by environmental_source_mapped
+dat5 %>%
+    ggplot(aes(x = lon, y = lat, color = environmental_source_mapped)) +
+    geom_point() +
+    theme_minimal()
+dat4 %>%
+    ggplot(aes(x = lon, y = lat, color = environmental_source_mapped)) +
+    geom_point() +
+    theme_minimal()
+
+
+# PROJECT NUMBERS OF INTEREST: 
+#PRJNA596592, n = 642 "Genomic analysis of coral associated bacteria in Hong Kong waters" Assign lat long from Hong Kong: Wong Wan Chau, with note "Hong Kong waters, lat long derived from biopackage information"
+#PRJNA391943 (tara, metagenome assembled genomes); PRJNA417962 (metagenome-assembled genomed (marine atlas project))
+
+
+
+#%>%
+#    select(
+#        taxon_id, assembly_accession, isolation_source, lat_lon, 
+#        isolation_source_mapped, environmental_source_mapped, lat, lon
+#        )
 
 # Save the data -----
 write_csv(dat3, here("data", "intermediate", "ncbi_metadata_clean.csv"))
