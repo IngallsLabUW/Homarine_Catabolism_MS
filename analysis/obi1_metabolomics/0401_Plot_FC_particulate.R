15ae023bd7f3bf7607441fb25c6ea52797382257
Katherine Heal <katherine.heal@pnnl.gov>
2025-06-10 11:51:35 -0700

    Add molecular formula and fix Fig SX1

--- analysis/obi1_metabolomics/0401_Plot_FC_particulate.R
+++ analysis/obi1_metabolomics/0401_Plot_FC_particulate.R
@@ -59,6 +59,35 @@ dat_enrich_summary <- dat_enrichment_results %>%
     # drop infinite values
     filter(med_fc_h != Inf & med_fc_hNH4 != Inf)
 
+# Subset to just mass_features that start with "\nRT"
+dat_enrich_summary_test <- dat_enrich_summary %>%
+    filter(med_pvalue_h < 0.05 & log2(med_fc_h) > 0) %>%
+  filter(str_detect(plot_label, "^\\nRT")) %>%
+    select(plot_label, mass_feature) %>%
+    distinct()
+
+# correct names on a few more
+# dat_enrich_summary <- dat_enrich_summary %>%
+#   mutate(
+#     plot_label = case_when(
+#       str_detect(plot_label, "\nRT = 9.6\n mz = 142.05-") ~ '"\\nRT = 9.6\\n mz = 142.05-\\n C"[6]*"H"[9]*"NO"[3]',
+#       str_detect(plot_label, "\nRT = 9.9\n mz = 142.05-") ~ "\nRT = 9.9\n mz = 142.05-\n C6H9NO3",
+#       str_detect(plot_label, "\nRT = 11.8\n mz = 160.061-") ~ "\nRT = 11.8\n mz = 160.061-\n C6H9NO4",
+#       str_detect(plot_label, "\nRT = 11.8\n mz = 188.056-") ~ "\nRT = 11.8\n mz = 188.056-\n C7H9NO5",
+#       TRUE ~ paste0('"', plot_label, '"')
+#     )
+#   )
+
+dat_enrich_summary <- dat_enrich_summary %>% 
+    mutate( 
+        plot_label = case_when( 
+            str_detect(plot_label, "\nRT = 9.6\n mz = 142.05-") ~ "\nRT = 9.6\n mz = 142.05-\n C6H9NO3", 
+            str_detect(plot_label, "\nRT = 9.9\n mz = 142.05-") ~ "\nRT = 9.9\n mz = 142.05-\n C6H9NO3", 
+            str_detect(plot_label, "\nRT = 11.8\n mz = 160.061-") ~ "\nRT = 11.8\n mz = 160.061-\n C6H9NO4", 
+            str_detect(plot_label, "\nRT = 11.8\n mz = 188.056-") ~ "\nRT = 11.8\n mz = 188.056-\n C7H9NO5",
+            str_detect(plot_label, "n-methyl glutamic acid") ~ "N-methylglutamic acid",
+            str_detect(plot_label, "n-methyl glutamine") ~ "N-methylglutamine",
+            TRUE ~ plot_label ) )
 
 set.seed(123)
 g4 <- ggplot(
@@ -83,7 +112,7 @@ g4 <- ggplot(
   theme_bw() +
   labs(
     x = "Median enrichment factor in homarine vs glucose (log2)",
-    y = "Median enrichment factor in homarine+glucose vs glucose (log2)",
+    y = "Median enrichment factor in glucose+homarine vs glucose (log2)",
   ) +
   scale_color_manual(values = c("aquamarine3", "darkorchid3", "black")) +
   scale_shape_manual(values = c(17, 16)) +
